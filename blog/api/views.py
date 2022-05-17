bb586ef0102a11bdae2f72be6b4a94affeb75388
halimzidoune <halimzidoune@gmail.com>
2022-05-17 22:04:15 +0100

    Post Crud

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from api import serializers
 from django.contrib.auth.models import User
+from api.models import Post
+
+###################### User ############################
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -9,3 +12,17 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+
+###################### Post ############################
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
